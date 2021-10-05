cdb9b7ff08a6581bf46716f48cb40a5b4bbac3c6
John Doe <johndoe@example.com>
2021-10-05 18:44:02 -0500

    commit 1

---
+++ Javascript/folder_structure/my-first-project/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <div>
+        Things I need to do:
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
